f78ee59d7d8ec407cfa2fd58c90c3900b5a4c61a
Alex Katz <alexander.k@strongroom.ai>
2023-08-08 08:33:27 +1000

    main- wip

---
+++ build.bat
@@ -0,0 +1,5 @@
+"C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Auxiliary\\Build\\vcvars64.bat"
+mkdir ..\build
+cd ..\build
+qmake ..\CI-CD-Public\CI-CD-App.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+nmake
