0f922467892fb1a638828703cee2c0c533cc75d2
Alex Katz <alexander.k@strongroom.ai>
2023-08-08 10:25:53 +1000

    main- wip

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
-"C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Auxiliary\\Build\\vcvars64.bat"
+#@echo off
+#setlocal
+
+#set PATH "%VC2019%;%PATH%"
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 mkdir ..\build
 cd ..\build
 qmake ..\CI-CD-Public\CI-CD-App.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
